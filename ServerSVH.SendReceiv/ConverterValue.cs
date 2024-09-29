2cbc1b3f17bf37ac630ad67629ae2d681b332b84
Lyudmer <ludmerekaterina@gmail.com>
2024-09-29 21:36:24 +0300

    добавлены файлы с общими функциями

---
+++ ServerSVH.SendReceiv/ConverterValue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ServerSVH.SendReceiv
+{
+    public class ConverterValue
+    {
+        public static T? ConvertTo<T>(string? valStr)
+        {
+            if (typeof(T) == typeof(int))
+            {
+                if (int.TryParse(valStr, out int result))
+                {
+                    return (T)Convert.ChangeType(result, typeof(T));
+                }
+            }
+            else if (typeof(T) == typeof(Guid))
+            {
+                if (Guid.TryParse(valStr, out Guid result))
+                {
+                    return (T)Convert.ChangeType(result, typeof(T));
+                }
+            }
+            else if (typeof(T) == typeof(DateTime))
+            {
+                if (DateTime.TryParse(valStr, out DateTime result))
+                {
+                    return (T)Convert.ChangeType(result, typeof(T));
+                }
+            }
+
+            return default; // Возвращаем значение по умолчанию, если преобразование не удалось
+        }
+    }
+}
---
+++ ServerSVH.SendReceiv/DopFunction.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ServerSVH.SendReceiv
+{
+    record class DocFromXml
+    (
+        string TypeDoc,
+        Guid DocId,
+        DateTime DocCreate,
+        string DocText
+    );
+    record class PkgFromXml
+    (
+        int Pid,
+        int Status,
+        Guid UserId,
+        Guid UUID
+    );
+    record class ResLoadPackage
+    (
+        int Pid,
+        int Status
+    );
+    public class DopFunction
+    {
+     
+       
+        public static string GetHashMd5(string text)
+        {
+            string result = string.Empty;
+            if (string.IsNullOrEmpty(text))
+            {
+                var md5 = MD5.Create();
+                var hash = md5?.ComputeHash(Encoding.UTF8.GetBytes(text));
+                if (hash != null) result = Convert.ToBase64String(hash);
+            }
+            return result;
+        }
+        public static string GetSha256(string text)
+        {
+            var sb = new StringBuilder();
+            using (var hash = SHA256.Create())
+            {
+                var result = hash?.ComputeHash(Encoding.UTF8.GetBytes(text));
+                if (result != null)
+                {
+                    for (int i = 0; i < result.Length; i++)
+                        sb.Append(result[i].ToString("x2"));
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
