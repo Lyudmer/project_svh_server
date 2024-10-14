54655d2a18f6a43f050130c8ff1569e906abaf1a
Lyudmer <ludmerekaterina@gmail.com>
2024-10-14 23:40:29 +0300

    fixed error mapping

---
+++ ServerSVH.DataAccess/Mapping/MapperProfile.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using ServerSVH.Core.Models;
+using ServerSVH.DataAccess.Entities;
+
+namespace ServerSVH.DataAccess.Mapping
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<DocumentEntity, Document>()
+                .ForAllMembers(x => x.Condition((src, dest, prop) =>
+                    {
+                        if (prop == null) return false;
+                        if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+                        return true;
+                     }
+                ));
+            CreateMap<PackageEntity, Package>()
+                .ForAllMembers(x => x.Condition((src, dest, prop) =>
+            {
+                if (prop == null) return false;
+                if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+                return true;
+            }
+                ));
+            CreateMap<StatusGraphEntity, StatusGraph>()
+                .ForAllMembers(x => x.Condition((src, dest, prop) =>
+                {
+                    if (prop == null) return false;
+                    if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+                    return true;
+                }
+                ));
+        }
+    }
+}
